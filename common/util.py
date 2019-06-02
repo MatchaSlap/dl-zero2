53ccd2f8b0fb07f1f7fada9bc901bded5a303aec
MatchaSlap <fa60393@gmail.com>
2019-06-02 14:12:12 +0900

    add

--- common/util.py
+++ common/util.py
@@ -16,6 +16,36 @@ def preprocess(text):
     corpus = np.array([word_to_id[w] for w in words])
     return corpus, word_to_id, id_to_word
 
+def eval_perplexity(model, corpus, batch_size=10, time_size=35):
+    print('evaluating perplexity ...')
+    corpus_size = len(corpus)
+    total_loss, loss_cnt = 0, 0
+    max_iters = (corpus_size - 1) // (batch_size * time_size)
+    jump = (corpus_size - 1) // batch_size
+
+    for iters in range(max_iters):
+        xs = np.zeros((batch_size, time_size), dtype=np.int32)
+        ts = np.zeros((batch_size, time_size), dtype=np.int32)
+        time_offset = iters * time_size
+        offsets = [time_offset + (i * jump) for i in range(batch_size)]
+        for t in range(time_size):
+            for i, offset in enumerate(offsets):
+                xs[i, t] = corpus[(offset + t) % corpus_size]
+                ts[i, t] = corpus[(offset + t + 1) % corpus_size]
+
+        try:
+            loss = model.forward(xs, ts, train_flg=False)
+        except TypeError:
+            loss = model.forward(xs, ts)
+        total_loss += loss
+
+        sys.stdout.write('\r%d / %d' % (iters, max_iters))
+        sys.stdout.flush()
+
+    print('')
+    ppl = np.exp(total_loss / max_iters)
+    return ppl
+
 def create_contexts_target(corpus, window_size=1):
     target = corpus[window_size:-window_size]
     contexts = []
@@ -68,6 +98,12 @@ def to_cpu(x):
     if type(x) == numpy.ndarray:
         return x
     return np.asnumpy(x)
+
+def to_gpu(x):
+    import cupy
+    if type(x) == cupy.ndarray:
+        return x
+    return cupy.asarray(x)
 
 def cos_similarity(x, y, eps=1e-8):
     nx = x / (np.sqrt(np.sum(x**2)) + eps)
